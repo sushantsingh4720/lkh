1c211334a889b52e721465a3e461670f8aae1c9a
Sushant Singh <sushantsingh4720@gmail.com>
2024-09-27 11:47:50 +0530

    view sales done

--- src/assets/helpers/CommonUses.tsx
+++ src/assets/helpers/CommonUses.tsx
@@ -1,1 +1,42 @@
 export const Curruncy: string = "₹";
+
+export const dateTransformDDMMYY = (date: string): string => {
+  const transformdate = date.split("-").reverse().join("/");
+  return transformdate;
+};
+
+export const getDueDateMessage = (dueDate: string): string => {
+  const currentDate = new Date(); // Get current date and time
+  currentDate.setHours(0, 0, 0, 0); // Set the time to midnight
+
+  const due = new Date(dueDate);
+  due.setHours(0, 0, 0, 0); // Set due date time to midnight
+
+  const timeDiff = due.getTime() - currentDate.getTime(); // Difference in milliseconds
+  const dayDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24)); // Convert to days
+
+  if (dayDiff === 0) {
+    return "today";
+  } else if (dayDiff === 1) {
+    return "tomorrow";
+  } else if (dayDiff > 1) {
+    return `in ${dayDiff} days`;
+  } else {
+    return "date passed";
+  }
+};
+
+export function padWithZeros(number: number): string {
+  return number.toString().padStart(5, "0");
+}
+
+export const parseFloatWithFixedValue = (value: string): string => {
+  return parseFloat(value).toFixed(2);
+};
+
+export function formatIndianCurrency(amount: number): string {
+  return amount.toLocaleString("en-IN", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+}
--- src/pages/invoices/sales/Sales.tsx
+++ src/pages/invoices/sales/Sales.tsx
@@ -3,12 +3,17 @@ import {
   IonContent,
   IonHeader,
   IonIcon,
+  IonItem,
   IonLabel,
+  IonList,
   IonPage,
+  IonText,
   IonTitle,
   IonToolbar,
+  useIonViewWillEnter,
+  useIonViewWillLeave,
 } from "@ionic/react";
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from "./Sales.module.scss";
 import {
   addOutline,
@@ -17,7 +22,43 @@ import {
   searchCircleSharp,
   searchOutline,
 } from "ionicons/icons";
+import useAxios from "../../../utils/axiosInstance";
+import LoadDataSpinner from "../../../components/Spinner/loadDataSpinner/LoadDataSpinner";
+import {
+  Curruncy,
+  dateTransformDDMMYY,
+  formatIndianCurrency,
+  getDueDateMessage,
+  padWithZeros,
+  parseFloatWithFixedValue,
+} from "../../../assets/helpers/CommonUses";
 const Sales: FC = () => {
+  const axios = useAxios();
+  const [items, setItems] = useState([]);
+  const [busy, setBusy] = useState<boolean>(false);
+
+  const generateItems = async () => {
+    setBusy(true);
+    try {
+      const response = await axios.get("/sales_inv");
+      const data = response.data;
+      setItems(data?.salesInvoices || []);
+    } catch (err: any) {
+      const error = err.response?.data;
+    } finally {
+      setBusy(false);
+    }
+  };
+  console.log(items);
+  // Use this hook to fetch items every time the page becomes active
+  useIonViewWillEnter(() => {
+    generateItems();
+  });
+
+  useIonViewWillLeave(() => {
+    setItems([]);
+  });
+
   return (
     <IonPage className={styles.salescontainer}>
       <IonHeader translucent={true}>
@@ -31,6 +72,47 @@ const Sales: FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
+        {busy ? (
+          <LoadDataSpinner />
+        ) : (
+          <IonList>
+            {items?.map((item: any, index: Number) => (
+              <IonItem key={item.id}>
+                <div className={styles.ionitem_container}>
+                  <div className={styles.first_line}>
+                    <IonLabel>{item?.name}</IonLabel>
+                    <IonLabel>
+                      {Curruncy}
+                      {formatIndianCurrency(item?.amount)}
+                    </IonLabel>
+                  </div>
+                  <div className={styles.second_line}>
+                    <div className={styles.second_line_first_component}>
+                      <IonText>{dateTransformDDMMYY(item?.date)}</IonText>
+                      <IonText> </IonText>
+                      <IonText>INV-{padWithZeros(item?.invoice)}</IonText>
+                    </div>
+                    {Number(item?.balance) ? (
+                      <div>
+                        Due:{Curruncy}
+                        {formatIndianCurrency(item?.balance)}
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                  {Number(item?.balance) ? (
+                    <div className={styles.due_color}>
+                      DUE {getDueDateMessage(item?.dueDate)}
+                    </div>
+                  ) : (
+                    <div className={styles.paid_color}>paid</div>
+                  )}
+                </div>
+              </IonItem>
+            ))}
+          </IonList>
+        )}
         <IonButton slot="fixed">
           <IonIcon icon={addOutline}></IonIcon>
         </IonButton>
